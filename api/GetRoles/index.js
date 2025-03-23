19f976c77f90d5a40e79c71a5266b51a7a10e00b
mchangchien <matt.changchien@microsoft.com>
2025-03-23 20:09:13 +0800

    revert

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,11 +1,3 @@
-const fetch = require('node-fetch').default;
-
-// add role names to this object to map them to group ids in your AAD tenant
-const roleGroupMappings = {
-    'admin': '0d89b60c-e78a-48e5-91a0-085af87eaa94',
-    'reader': '0d89b60c-e78a-48e5-91a0-085af87eaa94'
-};
-
 module.exports = async function (context, req) {
     const user = req.body || {};
     const roles = ["XXX"];
@@ -18,23 +10,4 @@ module.exports = async function (context, req) {
     context.res.json({
         roles
     });
-}
-
-async function isUserInGroup(groupId, bearerToken) {
-    const url = new URL('https://graph.microsoft.com/v1.0/me/memberOf');
-    url.searchParams.append('$filter', `id eq '${groupId}'`);
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${bearerToken}`
-        },
-    });
-
-    if (response.status !== 200) {
-        return false;
-    }
-
-    const graphResponse = await response.json();
-    const matchingGroups = graphResponse.value.filter(group => group.id === groupId);
-    return matchingGroups.length > 0;
 }
