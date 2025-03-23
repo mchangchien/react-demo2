290c6c1078771ee394965f82c5bbd5e1869ef018
mchangchien <matt.changchien@microsoft.com>
2025-03-23 20:12:01 +0800

    ddd

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     const user = req.body || {};
-    const roles = ["XXX"];
+    const roles = ["XXX", "YYY"];
     
     // for (const [role, groupId] of Object.entries(roleGroupMappings)) {
     //     if (await isUserInGroup(groupId, user.accessToken)) {
